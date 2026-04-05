edf4b470870c24e459ea2c9f444f858ba9e54435
mardbrito <marcelo4web@gmail.com>
2026-04-05 15:10:20 +0800

    improve header and add logout

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,6 +1,24 @@
-import { Link } from "@tanstack/react-router";
+import { authClient } from "@/lib/auth-client";
+import { Link, useRouter } from "@tanstack/react-router";
+import { auth } from "../../../server/lib/auth";
 
 export default function Header() {
+  const router = useRouter();
+  const { data: session, isPending } = authClient.useSession();
+
+  const handleLogin = () => {
+    router.navigate({ to: "/login" });
+  };
+
+  const handleLogOut = async () => {
+    try {
+      await authClient.signOut();
+      handleLogin();
+    } catch (error) {
+      console.error("Logout failed: ", error);
+    }
+  };
+
   return (
     <div className="sticky top-0  z-50 w-full">
       <div className="container mx-auto flex justify-between items-center">
@@ -11,14 +29,33 @@ export default function Header() {
         >
           Home
         </Link>
-        <nav>
+        <nav className="flex items-center gap-2">
           <Link
             to="/todos"
             activeProps={{ className: "font-bold text-blue-500" }}
-            className="text-xl font-bold "
+            className={`${!session ? "opacity-50 cursor-not-allowed" : "text-xl font-bold"}`}
+            disabled={!session}
           >
             Todos
           </Link>
+
+          {isPending ? null : session ? (
+            <button
+              aria-label="Logout"
+              className="btn btn-ghost btn-sm"
+              onClick={handleLogOut}
+            >
+              Bye
+            </button>
+          ) : (
+            <button
+              aria-label="Login"
+              className="btn btn-ghost btn-sm"
+              onClick={handleLogin}
+            >
+              Logar
+            </button>
+          )}
         </nav>
       </div>
     </div>
